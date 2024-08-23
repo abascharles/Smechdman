405b773825d1e72fa7c4785b58eb6aa914a302dd
abascharles <abascharlesbenard@gmail.com>
2024-08-23 10:27:56 +0300

    The Switch Statement

--- 01-Fundamentals-Part-1/starter/scirpt.js
+++ 01-Fundamentals-Part-1/starter/scirpt.js
@@ -217,7 +217,7 @@ if (favorite === 23) {
 if (favorite !== 23) {
   console.log("Why not 23?");
 }
-*/
+
 
 // Boolean logic/ logic operators
 const hasDriversLicense = true;
@@ -256,3 +256,44 @@ if (scoreDolphins > scoreKoalas) {
 } else if (scoreDolphins === scoreKoalas) {
   console.log("Both win the trophy");
 }
+
+
+// The Switch statement
+const day = "monday";
+
+switch (day) {
+  case "monday":
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend");
+    break;
+
+  default:
+    console.log("Not a valid Day");
+}
+
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || _day === "thursady") {
+  console.log("Write code examples");
+}
+*/
+
+//
