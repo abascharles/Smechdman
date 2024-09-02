45e2c6ac93e62176a9c5e4b5c9218d38631c5eb6
abascharles <abascharlesbenard@gmail.com>
2024-09-02 21:48:53 +0300

    Intro to While Loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -432,10 +432,22 @@
 // }
 
 // Nested loop
-for (let exercise = 1; exercise < 4; exercise++) {
-  console.log(`--------Starting exercise ${exercise}`);
+// for (let exercise = 1; exercise < 4; exercise++) {
+//   console.log(`--------Starting exercise ${exercise}`);
 
-  for (let rep = 1; rep < 6; rep++) {
-    console.log(`Lifting weight repetition ${rep}🏋🏾`);
-  }
+//   for (let rep = 1; rep < 6; rep++) {
+//     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}🏋🏾`);
+//   }
+// }
+
+////////////////////
+//While Loop
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}🏋🏾`);
 }
+
+let rep = 1;
+while (rep <= 10) {
+  console.log(`WHILE: Lifting weights repetition ${rep}🏋🏾`);
+}
+rep++;
