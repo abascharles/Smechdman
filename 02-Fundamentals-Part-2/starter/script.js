84af277dc9429ac55569c11621ae0e69a1869c0a
abascharles <abascharlesbenard@gmail.com>
2024-09-02 07:14:56 +0300

    Challange #3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -347,20 +347,10 @@ john.calcBMI();
 
 if (mark.bmi > john.bmi) {
   console.log(
-    `${mark.fullName}'s BMI (${mark.bmi.toFixed(1)}) is higher than ${
-      john.fullName
-    }'s (${john.bmi.toFixed(1)})!`
-  );
-} else if (john.bmi > mark.bmi) {
-  console.log(
-    `${john.fullName}'s BMI (${john.bmi.toFixed(1)}) is higher than ${
-      mark.fullName
-    }'s (${mark.bmi.toFixed(1)})!`
+    `${mark.fullName}'s BMI ${mark.bmi} is higher than ${john.fullName}'s BMI ${john.bmi}`
   );
 } else {
   console.log(
-    `${mark.fullName} and ${
-      john.fullName
-    } have the same BMI (${mark.bmi.toFixed(1)})!`
+    `${john.fullName}'s BMI ${john.bmi} is higher than ${mark.fullName}'s BMI ${mark.bmi}`
   );
 }
