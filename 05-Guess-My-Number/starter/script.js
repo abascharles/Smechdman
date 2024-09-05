b783df3b62171d089696c06f0109b8ed571fe190
abascharles <abascharlesbenard@gmail.com>
2024-09-05 10:17:45 +0300

    Event Handling and The DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉Correct Number!; '
+
+
+// document.querySelector('.number').textContent = '13'
+// document.querySelector('.score').textContent = '10'
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+// Listening to events
+document.querySelector('.check').addEventListener('click', function(){
+   const guess = Number(document.querySelector('.guess').value) ;
+
+
+   if(!guess ){
+    document.querySelector('.message').textContent = '⛔No number!'
+   }
+})
