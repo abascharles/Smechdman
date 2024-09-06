c8b03bfee7ed542b15efcf310902abdf53d551cf
abascharles <abascharlesbenard@gmail.com>
2024-09-06 22:36:12 +0300

    Implementing first Scenarion

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,9 +1,7 @@
 'use strict';
 
-
 // console.log(document.querySelector('.message').textContent);
 // document.querySelector('.message').textContent = '🎉Correct Number!; '
-
 
 // document.querySelector('.number').textContent = '13'
 // document.querySelector('.score').textContent = '10'
@@ -12,11 +10,10 @@
 // console.log(document.querySelector('.guess').value);
 
 // Listening to events
-document.querySelector('.check').addEventListener('click', function(){
-   const guess = Number(document.querySelector('.guess').value) ;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
 
-
-   if(!guess ){
-    document.querySelector('.message').textContent = '⛔No number!'
-   }
-})
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!⛔';
+  }
+});
