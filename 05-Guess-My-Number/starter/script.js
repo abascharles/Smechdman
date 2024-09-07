9ffbc66d278e5c2aab0a9be4b03834e57b113833
abascharles <abascharlesbenard@gmail.com>
2024-09-07 09:49:56 +0300

    Implementing the game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,14 +9,49 @@
 // document.querySelector('.score').textContent = '10'
 
 // document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
+// console.log(document.querySelector('.guess').value);\
+
+///////////////////////////////////////////////////////
+// Secret Number
+const secretNumber = Math.trunc(Math.random() *20) +1
+
+// Declaring the score to inc/dec later
+let score = 20
+document.querySelector('.number').textContent = secretNumber
 
 // Listening to events
 document.querySelector('.check').addEventListener('click', function(){
-   const guess = Number(document.querySelector('.guess').value) ;
+ const guess = Number(document.querySelector('.guess').value);
 
+// Implementing the game logic
+ if(!guess){
+   document.querySelector('.message').textContent = 'No number!⛔'
 
-   if(!guess ){
-    document.querySelector('.message').textContent = '⛔No number!'
+ }else if(guess === secretNumber){
+   document.querySelector('.message').textContent = '🎉Correct Number!'
+
+ }else if(guess > secretNumber){
+   if(score > 1 ){
+   document.querySelector('.message').textContent = '📈 Too High!'
+   // decresing the score and displaying it. 
+   score--
+   document.querySelector('.score').textContent = score
+   }else{
+   document.querySelector('.message').textContent = '💥You Lost the game'
+   document.querySelector('.score').textContent = 0
    }
-})
+
+   
+ }else if(guess < secretNumber){
+     if(score > 1 ){
+   document.querySelector('.message').textContent = '📉 Too Low!'
+   // decresing the score and displaying it. 
+   score--
+   document.querySelector('.score').textContent = score
+   }else{
+   document.querySelector('.message').textContent = '💥You Lost the game'
+   document.querySelector('.score').textContent = 0
+   }
+ }
+});
+
