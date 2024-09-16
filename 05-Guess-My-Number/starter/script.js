aa2f156b197eb54164e743595ac76f83e5b11e38
abascharles <abascharlesbenard@gmail.com>
2024-09-16 11:11:24 +0300

    DRY code

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -41,30 +41,43 @@ if(score > highscore){
   document.querySelector('.highscore').textContent = highscore
 }
 
-// When guess is too high
- }else if(guess > secretNumber){
-   if(score > 1 ){
-   document.querySelector('.message').textContent = '📈 Too High!'
-   // decreasing the score and displaying it. 
-   score--
-   document.querySelector('.score').textContent = score
-   }else{
-   document.querySelector('.message').textContent = '💥You Lost the game'
-   document.querySelector('.score').textContent = 0
-   }
+// When guess is diffrent
+ }else if(guess !== secretNumber){
+  if(score > 1 ){
+     document.querySelector('.message').textContent = guess > secretNumber? '📈 Too High!': '📉 Too Low!'
+    // decresing the score and displaying it. 
+    score--
+    document.querySelector('.score').textContent = score
+    }else{
+    document.querySelector('.message').textContent = '💥You Lost the game'
+    document.querySelector('.score').textContent = 0
+    }
 
-  // When guess is too low
- }else if(guess < secretNumber){
-     if(score > 1 ){
-   document.querySelector('.message').textContent = '📉 Too Low!'
-   // decresing the score and displaying it. 
-   score--
-   document.querySelector('.score').textContent = score
-   }else{
-   document.querySelector('.message').textContent = '💥You Lost the game'
-   document.querySelector('.score').textContent = 0
-   }
  }
+//  // When guess is too high
+//  else if(guess > secretNumber){
+//    if(score > 1 ){
+//   document.querySelector('.message').textContent = '📉 Too Low!'
+//    // decreasing the score and displaying it. 
+//    score--
+//    document.querySelector('.score').textContent = score
+//    }else{
+//    document.querySelector('.message').textContent = '💥You Lost the game'
+//    document.querySelector('.score').textContent = 0
+//    }
+
+//   // When guess is too low
+//  }else if(guess < secretNumber){
+//      if(score > 1 ){
+//    document.querySelector('.message').textContent = '📉 Too Low!'
+//    // decresing the score and displaying it. 
+//    score--
+//    document.querySelector('.score').textContent = score
+//    }else{
+//    document.querySelector('.message').textContent = '💥You Lost the game'
+//    document.querySelector('.score').textContent = 0
+//    }
+//  }
 });
 
 // Resetting the game
