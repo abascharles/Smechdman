4fe7e2ddc6792c1f1ddc3065abb0434e2274bc96
abascharles <abascharlesbenard@gmail.com>
2024-09-18 14:28:39 +0300

    handling key press events

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,19 +1,27 @@
 'use strict';
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-const openModal = function(){
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-}
-const closeModal = function(){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-}
-for(let i = 0; i <= btnsOpenModal.length; i++){
-    btnsOpenModal[i].addEventListener('click', openModal )
-    btnCloseModal.addEventListener('click',closeModal )
-    overlay.addEventListener('click', closeModal)
-}
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// Keyboard Events - global events as they do not happen on one element only
+document.addEventListener('keydown', function(e){
+    console.log(e);
+}) 
