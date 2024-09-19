41b7d8fe509b575d0c53fb3cdd418e1a78ef6e5a
abascharles <abascharlesbenard@gmail.com>
2024-09-19 09:57:45 +0300

    started the project pig game implementation

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Setting conditions
+score0El.textContent = 0;
+score1El.textContent = 0; 
+diceEl.classList.add('hidden')
+
+ 
