ea89e2723a2fa565bb72d9cf38e0b9380fa24743
abascharles <abascharlesbenard@gmail.com>
2024-09-21 09:43:19 +0300

    adding dice to the current score

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,12 +3,16 @@
 // Selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0')
+const current1El = document.getElementById('current--1')
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--new')
 const player1 = document.querySelector('.player--0')
 const player2 = document.querySelector('.player--1')
+let currentScore = 0
 
 // Setting conditions
 score0El.textContent = 0;
@@ -26,10 +30,17 @@ btnRoll.addEventListener('click', function(){
     // 2. Display the dice 
     diceEl.classList.remove('hidden');
     // manipulating the source el from the html
-    diceEl.src = `dice-${dice}.png`  
+    diceEl.src = `dice-${dice}.png`
+    console.log(dice);  
 
-    // 3. Checked for rolled 1: if true, switch to the next player
-    if(dice = 1){
+    // 3. Checked for rolled 1: if true, 
+    if(dice !== 1){
+        // Add dice to the currents score
+        currentScore += dice;
+        current0El.textContent = currentScore
+    
+    }else{
+        // switch to the next player
         player1.classList.remove('player--active')
         player2.classList.add('player--active')
     }
