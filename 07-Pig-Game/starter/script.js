20c08d53a737ce2c7b6a5bd8af0d52ef8a3e8199
abascharles <abascharlesbenard@gmail.com>
2024-09-21 09:23:01 +0300

    Displaying the dice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -7,10 +7,8 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--new')
-
-// Generating a randon number for the dice roll.
-const dice = Math.trunc(Math.random() * 6 + 1)
-console.log(dice)
+const player1 = document.querySelector('.player--0')
+const player2 = document.querySelector('.player--1')
 
 // Setting conditions
 score0El.textContent = 0;
@@ -18,10 +16,23 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 
-// Rolling the dice
+// Rolling the dice functionality
+
 btnRoll.addEventListener('click', function(){
+   
+    // 1. generating random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1
+
+    // 2. Display the dice 
     diceEl.classList.remove('hidden');
-     
+    // manipulating the source el from the html
+    diceEl.src = `dice-${dice}.png`  
+
+    // 3. Checked for rolled 1: if true, switch to the next player
+    if(dice = 1){
+        player1.classList.remove('player--active')
+        player2.classList.add('player--active')
+    }
 })
 
  
