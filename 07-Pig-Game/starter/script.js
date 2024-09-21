0e9cafd58ccf60e016dfca6d1fb51a2fa6c79d3f
abascharles <abascharlesbenard@gmail.com>
2024-09-21 10:39:34 +0300

    switching active player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting Elements
+const player0El = document.querySelector('.player--0')
+const player1EL = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0')
@@ -9,9 +11,8 @@ const current1El = document.getElementById('current--1')
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
-const btnHold = document.querySelector('.btn--new')
-const player1 = document.querySelector('.player--0')
-const player2 = document.querySelector('.player--1')
+const btnHold = document.querySelector('.btn--hold')
+
 const scores = [0, 0]
 let currentScore = 0
 let activePlayer = 0
@@ -27,7 +28,7 @@ diceEl.classList.add('hidden');
 
 btnRoll.addEventListener('click', function(){
    
-    // 1. generating random dice roll
+    // 1. Generating random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1
 
     // 2. Display the dice 
@@ -44,13 +45,16 @@ btnRoll.addEventListener('click', function(){
        
     }else{
         // switch to the next player
-           //setting text content to 0
+        //setting text content to 0
         document.getElementById(`current--${activePlayer}`).textContent = 0;
         currentScore = 0;
+
+        // switching the players
         activePlayer = activePlayer === 0 ? 1 : 0
     
-        player1.classList.remove('player--active')
-        player2.classList.add('player--active')
+        player0El.classList.toggle('player--active')
+        player1EL.classList.toggle('player--active')
+    
     }
 })
 
